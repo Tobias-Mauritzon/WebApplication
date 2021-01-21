c0b888433fb6ce228d4e2f250e0c833d5d9dd349
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-01-21 17:00:53 +0100

    glider_game prototype

--- Lab 2/lab2 Matteus/src/main/webapp/js/glider_script.js
+++ Lab 2/lab2 Matteus/src/main/webapp/js/glider_script.js
@@ -3,23 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var canvas;
+var ctx;
 
 $(document).ready(function(){
-    var running = false;
-    $("#rotateButton").click(function(){
-        if(!running){
-            $(".box").css("animation-play-state","running");
-            $("#rotateButton").text("Stop Rotating");
-            running = true;
-        }
-        else{
-            $(".box").css("animation-play-state","paused");
-            $("#rotateButton").text("Rotate!");
-            running = false;
-        }
+       
+    $("#start_button").click(function(){
+        $("p").css("color", "red");
+        canvas = document.getElementById('background_canvas');
+        ctx = canvas.getContext("2d");
+        spawn_background();
     })
     
   
   }); 
 
+function spawn_background(){
+    ctx.drawImage("../Resources/glider_res/terrang6.png", 0, 0);
+}
 
