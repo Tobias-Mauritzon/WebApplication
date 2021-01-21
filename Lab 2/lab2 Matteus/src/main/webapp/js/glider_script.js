d54051cd12858817913ba487d7cda3d59a65e2b0
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-01-21 16:14:01 +0100

    uppdate to glider test

--- Lab 2/lab2 Matteus/src/main/webapp/js/glider_script.js
+++ Lab 2/lab2 Matteus/src/main/webapp/js/glider_script.js
@@ -4,4 +4,22 @@
  * and open the template in the editor.
  */
 
+$(document).ready(function(){
+    var running = false;
+    $("#rotateButton").click(function(){
+        if(!running){
+            $(".box").css("animation-play-state","running");
+            $("#rotateButton").text("Stop Rotating");
+            running = true;
+        }
+        else{
+            $(".box").css("animation-play-state","paused");
+            $("#rotateButton").text("Rotate!");
+            running = false;
+        }
+    })
+    
+  
+  }); 
+
 
