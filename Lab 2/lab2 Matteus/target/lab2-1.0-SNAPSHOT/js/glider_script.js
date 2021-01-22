5fc2f18b9515e13db6af3f435858626c24b45cd5
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-01-22 11:51:15 +0100

    maps update correctly

--- Lab 2/lab2 Matteus/target/lab2-1.0-SNAPSHOT/js/glider_script.js
+++ Lab 2/lab2 Matteus/target/lab2-1.0-SNAPSHOT/js/glider_script.js
@@ -5,12 +5,13 @@
  */
 var canvas;
 var ctx;
-var my_image;
+var background_image;
 var pawn_canvas;
 var pawn_ctx;
 var pawn;
 var pawn_pos_x;
 var pawn_pos_y;
+var speed = 5;
 
 $(document).ready(function(){
        
@@ -22,8 +23,8 @@ $(document).ready(function(){
         // Initializing canvas
         canvas = document.getElementById('background_canvas');
         ctx = canvas.getContext('2d');
-        my_image = new Image(1000, 700);
-        my_image.src = "Resources/glider_res/terrang6.png";
+        background_image = new Image(1000, 700);
+        background_image.src = "Resources/glider_res/terrang4.png";
         spawn_background();
         
         // Add keyboard events for movement
@@ -36,17 +37,15 @@ $(document).ready(function(){
         
         pawn_image = new Image(64, 64);
         pawn_image.src = "Resources/glider_res/gubbe2.png";
-        spawn_pawn();
-         
-         
+        spawn_pawn();        
          
     });    
   
   }); 
 
 function spawn_background(){
-    my_image.onload = function() {
-        ctx.drawImage(my_image, 0, 0);
+    background_image.onload = function() {
+        ctx.drawImage(background_image, 0, 0);
     } 
 }
 function spawn_pawn(){
@@ -60,36 +59,74 @@ function spawn_pawn(){
 function key_press(event){
     $('#h1_g').html('keydown() is triggered!, keyCode = ' 
               + event.keyCode + ' which = ' + event.which);
-      //A
-      if(event.which === 65){
-          pawn_pos_x += -5;
-          pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y);
-      }
-      
-      //W
-      if(event.which === 87){
-          pawn_pos_y += -5;
-          pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y);
-      }
-      
-      //D
-      if(event.which === 68){
-          pawn_pos_x += 5;
-          pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y);
-      }
-      
-      //S
-      if(event.which === 83){
-          pawn_pos_y += 5;
-          pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y);
-      }
+    //A
+    if(event.which === 65){
+        pawn_pos_x += -speed;
+        change_backround();
+    }
+
+    //W
+    if(event.which === 87){
+        pawn_pos_y += -speed;
+        change_backround();
+    }
+
+    //D
+    if(event.which === 68){
+        pawn_pos_x += speed;
+        change_backround();
+    }
+
+    //S
+    if(event.which === 83){
+        pawn_pos_y += speed;
+        
+        change_backround();
+    }
       
       //R
-      if(event.which === 82){
+    if(event.which === 82){
         pawn_pos_x = 0;
         pawn_pos_y = 0;
-        pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y);
-      }
+    }
+    
+    // I
+    if(event.which === 73){
+        speed += 5;
+    }
+    
+    // U
+    if(event.which === 85){
+        speed += -5;
+    }
+    
+    pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y);
 }
 
-//"../Resources/glider_res/terrang6.png"
+function re_draw_background(){
+    var map = Math.floor(Math.random() * 4) + 1;
+    background_image.src = "Resources/glider_res/terrang"+map+".png";
+    ctx.drawImage(background_image, 0, 0);
+}
+
+function change_backround(){
+    if(pawn_pos_x + 64 > 1000){
+        re_draw_background();
+        pawn_pos_x += -1000;
+    }
+    
+    if(pawn_pos_x + 64 < 0){
+        re_draw_background();
+        pawn_pos_x += 1000;
+    }
+    
+    if(pawn_pos_y + 64 > 700){
+        re_draw_background();
+        pawn_pos_y += -700;
+    }
+    
+    if(pawn_pos_y + 64 < 0){
+        re_draw_background();
+        pawn_pos_y += 700;
+    }
+}
