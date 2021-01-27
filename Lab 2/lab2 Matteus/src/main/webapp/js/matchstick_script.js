6711c1e8603927ff9378c00831b0b8cd9fde6ee7
Joachim <60391253+joantfolk@users.noreply.github.com>
2021-01-27 13:12:08 +0100

    bättre matchstick
    bättre matchstick

--- Lab 2/lab2 Matteus/src/main/webapp/js/matchstick_script.js
+++ Lab 2/lab2 Matteus/src/main/webapp/js/matchstick_script.js
@@ -1,23 +1,71 @@
-var count = 3;
+var count;
+var canvas;
+var ctx;
+var background_image;
+var match;
+var matches;
 
 $(document).ready(function(){
     
+    // Initializing
+    canvas = document.getElementById("gameCanvas");
+    ctx = canvas.getContext("2d");
+    background_image = new Image(1000, 600);
+    background_image.src = "Resources/matchstick/table.png";
+    ctx.drawImage(background_image, 0, 0);
+
+    reset();
+    spawnBackground();
+    
     $("#removeOne").click(function(){
-       $("#stick" + count + "").hide();
-       count -= 1; 
+       removeOne();
+       Redraw();
     });
     
     $("#removeTwo").click(function(){
-        for(i=0; i<2; i++){
-            $("#stick" + count + "").hide();
-            count -= 1;
-        } 
+        removeTwo();
+        Redraw();
     });
     
     $("#reset").click(function(){
-        for(i=1; i<4; i++){
-            $("#stick" + i + "").show();
-        }
-        count = 3;
+        reset();
+        Redraw();
     });
 });
+
+function spawnBackground(){
+    background_image.onload = function() {
+        ctx.drawImage(background_image, 0, 0);    
+        for(i = 0; i < count; i++){
+            ctx.drawImage(matches[i].image, matches[i].x, matches[i].y);
+        }
+    };
+}
+
+function Redraw(){
+    ctx.drawImage(background_image, 0, 0);    
+    for(i = 0; i < count; i++){
+        ctx.drawImage(matches[i].image, matches[i].x, matches[i].y);
+    }
+}
+
+function removeOne(){
+    count -= 1;
+}
+
+function removeTwo(){
+    count -= 2;
+}
+
+function reset(){
+    count = 0;
+    matches = [];
+    for(i = 0; i < 21; i++){
+        match = new Image(40, 140);
+        match.src = "Resources/matchstick/matchstick.png";
+        var xPos = Math.random() * 900;
+        var yPos = Math.random() * 450;
+        matches.push({image:match, x: xPos, y:yPos});
+    }
+    count = 21;
+}
