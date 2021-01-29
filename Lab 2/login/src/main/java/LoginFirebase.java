0a8baeecc990aef0388a98358857c499b867e8c6
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-01-29 14:54:35 +0100

    Added database json to ignore and connection tests

--- Lab 2/login/src/main/java/LoginFirebase.java
+++ Lab 2/login/src/main/java/LoginFirebase.java
@@ -2,9 +2,11 @@
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,13 +23,18 @@ public class LoginFirebase {
     public String login() throws FileNotFoundException, IOException{
         
         String res = "fail";
-        String conString =  System.getenv("Firebase_login");
-        
-        System.out.println(conString);
         
         //JSONObject json = new JSONObject(conString);  
         
-        try( FileInputStream serviceAccount = new FileInputStream("conString");){
+        String dirName = "../applications/__internal/login-1.0-SNAPSHOT/";
+
+        Files.list(new File(dirName).toPath())
+                .limit(20)
+                .forEach(path -> {
+                    System.out.println(path);
+                });
+       
+        try( FileInputStream serviceAccount = new FileInputStream("../resources/application-firebase-admin.json");){
 
             FirebaseOptions options = new FirebaseOptions.Builder()
               .setCredentials(GoogleCredentials.fromStream(serviceAccount))
