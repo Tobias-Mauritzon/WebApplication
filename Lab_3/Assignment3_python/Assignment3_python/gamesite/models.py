666c4c828da1c4f56bf7449f339c585af895b65b
Lerbyn <lerbyn98@gmail.com>
2021-02-05 11:49:21 +0100

    Add ORM for game and comment
    Code can be found in gamesite/models.py
    To apply to database run python manage.py makemigrations then python manage.py migrate
    User doesn't seem necessary since it is built in to Django

--- Lab_3/Assignment3_python/Assignment3_python/gamesite/models.py
+++ Lab_3/Assignment3_python/Assignment3_python/gamesite/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Doesnt seem to be necessary since Django has built in users with email username and password
+# class User(models.Model):
+#     username = models.CharField(max_length=20, primary_key=True)
+#     mail = models.CharField(max_length=200, unique=True)
+#     password = models.CharField(max_length=20)
+
+class Game(models.Model):
+    GameName = models.CharField(max_length=50, primary_key=True)
+
+class Comment(models.Model):
+    Id = models.IntegerField(primary_key=True)
+    commentText = models.TextField()
+    timeStamp = models.DateTimeField(default=timezone.now)
