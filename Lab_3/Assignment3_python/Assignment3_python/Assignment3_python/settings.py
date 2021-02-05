80b5d6f28f1a401ddbeb104274282d50b34c9eca
Lerbyn <lerbyn98@gmail.com>
2021-02-05 11:08:13 +0100

    Security fix Django secret key is no longer plain text on repo
    Django SECRET_KEY is now read from either enviromental variables on system or from a json file which will not be version controlled under git. reading a json file is not yet implemented and the SECRET_KEY will be read from system variable DJANGO_SECRET_KEY

--- Lab_3/Assignment3_python/Assignment3_python/Assignment3_python/settings.py
+++ Lab_3/Assignment3_python/Assignment3_python/Assignment3_python/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import pprint 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nr_wm#=5@51=6%6yi7(a*__=@r^rqm9na&pkt=#))673crb&js'
-
+if os.path.exists('secrets.json'):
+    SECRET_KEY = '' # read secrets from json file json file not implemented
+else:
+    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+    # SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
