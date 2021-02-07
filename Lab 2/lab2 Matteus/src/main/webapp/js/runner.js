052404ba1dba6bd9a878471cd7a2fa36bc06102a
Game9954 <60510013+Game9954@users.noreply.github.com>
2021-02-07 16:50:32 +0100

    update lab 2 mockup with more reactive layout and update runner game

---
+++ Lab 2/lab2 Matteus/src/main/webapp/js/runner.js
@@ -0,0 +1,121 @@
+// //@ts-check
+var runner;
+var canvas;
+var canvas_width = 700;
+var canvas_height = 700;
+var ctx;
+var player;
+var obstacle;
+var background;
+
+$(document).ready(function(){
+  canvas = document.getElementById("canvas");
+  player = new Player();
+  obstacle = new Obstacle();
+  background = new Background();
+
+  $("#start_button").click(function(){
+
+    ctx = canvas.getContext("2d");
+
+    document.addEventListener('keydown', (e) => {
+      var key = e.which;
+      if(key == 32)  // the space key code
+       {
+        player.jump();
+       }
+    })
+
+    setInterval(onTimerTick, 33); // 33 milliseconds = ~ 30 frames per sec
+
+    function onTimerTick() {
+        player.update();
+        obstacle.update();
+
+        background.draw();
+        player.draw();
+        obstacle.draw();
+    }
+  });
+});
+
+function Vector(x,y,dx,dy){
+  // position
+  this.x = x || 0;
+  this.y = y || 0;
+  // direction
+  this.dx = dx || 0;
+  this.dy = dy || 0;
+}
+
+/**
+ * Advance the vectors position by dx,dy
+ */
+Vector.prototype.advance = function() {
+    this.x += this.dx;
+    this.y += this.dy;
+  };
+
+
+function Player() {
+  this.width = 100;
+  this.height = 100;
+  this.gravity = 1;
+  this.jumpVelocity = 20;
+  this.image = new Image(this.width,this.height);
+  this.image.src = "Resources/squirrel.png";
+  this.vector = new Vector(200,600,0,0);
+}
+
+Player.prototype.update = function(){
+  if(this.vector.y < 700 -this.height){
+    this.vector.dy += this.gravity;
+  }
+  if(this.vector.y > (700 -this.height)){
+    this.vector.dy = 0;
+    this.vector.y = 700 -this.height;
+  }
+
+  this.vector.advance();
+
+}
+
+Player.prototype.jump = function(){
+  if(this.vector.dy == 0){
+    this.vector.dy = -this.jumpVelocity;
+  }
+}
+
+Player.prototype.draw = function(){
+  ctx.drawImage(this.image,this.vector.x,this.vector.y,this.width,this.height);
+}
+
+
+function Obstacle() {
+  this.width = 80;
+  this.height = 80;
+  this.image = new Image(this.width,this.height);
+  this.image.src = "Resources/matchstick/table.png";
+  this.vector = new Vector(700,700-this.height,-10,0);
+}
+
+Obstacle.prototype.update = function(){
+  if(this.vector.x <= 0){
+    this.vector.x = 700
+  }
+  this.vector.advance();
+}
+
+Obstacle.prototype.draw = function(){
+  ctx.drawImage(this.image,this.vector.x,this.vector.y,this.width,this.height);
+}
+
+
+function Background() {
+  this.image = new Image(this.width,this.height);
+  this.image.src = "Resources/windows.jpg";
+}
+
+Background.prototype.draw = function(){
+  ctx.drawImage(this.image,0,0);
+}
