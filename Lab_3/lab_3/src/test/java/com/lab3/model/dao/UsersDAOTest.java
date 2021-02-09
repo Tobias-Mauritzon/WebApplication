99afbae390fad87cee32a80311c53dbacd5675e3
SlaughterBrute <wille.j@gmail.com>
2021-02-09 14:56:40 +0100

    Added create, find and remove user tests to UsersDAOTest

--- Lab_3/lab_3/src/test/java/com/lab3/model/dao/UsersDAOTest.java
+++ Lab_3/lab_3/src/test/java/com/lab3/model/dao/UsersDAOTest.java
@@ -9,6 +9,7 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,23 +25,56 @@ public class UsersDAOTest {
                     .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
+    Users user1;
+    Users user2;
+    
     @EJB
     private UsersDAO usersDAO;
 
     @Before
     public void init() {
+        user1 = new Users("mail1", "name1", "password1");
+        user2 = new Users("mail2", "name2", "password2");
     }
 
     @Test
-    public void create_users() {
-        Users user1 = new Users("mail1", "name1", "password1");
-        Users user2 = new Users("mail2", "name2", "password2");
+    public void createUsers() {
+        Assert.assertEquals(0, usersDAO.findUsersMatchingName("mail1").toArray().length);
+        Assert.assertEquals(0, usersDAO.findUsersMatchingName("mail2").toArray().length);
+        usersDAO.create(user1);
+        Assert.assertEquals(1, usersDAO.findUsersMatchingName("mail1").toArray().length);
+        Assert.assertEquals(0, usersDAO.findUsersMatchingName("mail2").toArray().length);
+        usersDAO.create(user2);
+        Assert.assertEquals(1, usersDAO.findUsersMatchingName("mail1").toArray().length);
+        Assert.assertEquals(1, usersDAO.findUsersMatchingName("mail2").toArray().length);
+    }
+    
+    @Test
+    public void findUsers(){
         usersDAO.create(user1);
         usersDAO.create(user2);
+        
         Users[] expected1 = {user1};
         Assert.assertArrayEquals(expected1, usersDAO.findUsersMatchingName("mail1").toArray());
         Users[] expected2 = {user2};
         Assert.assertArrayEquals(expected2, usersDAO.findUsersMatchingName("mail2").toArray());
+    }
+    
+    @Test
+    public void removeUsers(){
+        usersDAO.create(user1);
+        usersDAO.create(user2);
+        
+        usersDAO.remove(user1);
+        Assert.assertEquals(0, usersDAO.findUsersMatchingName("mail1").toArray().length);
+        Assert.assertEquals(1, usersDAO.findUsersMatchingName("mail2").toArray().length);
+        usersDAO.remove(user2);
+        Assert.assertEquals(0, usersDAO.findUsersMatchingName("mail1").toArray().length);
+        Assert.assertEquals(0, usersDAO.findUsersMatchingName("mail2").toArray().length);
+    }
+    
+    @After
+    public void end(){
         usersDAO.remove(user1);
         usersDAO.remove(user2);
     }
