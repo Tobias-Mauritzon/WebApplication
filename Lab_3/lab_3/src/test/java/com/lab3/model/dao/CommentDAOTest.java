acc671250820d7c19b94fbcdb9e76ff797b9457a
Game9954 <60510013+Game9954@users.noreply.github.com>
2021-02-10 15:41:22 +0100

    Add tests to CommentDAOTest

--- Lab_3/lab_3/src/test/java/com/lab3/model/dao/CommentDAOTest.java
+++ Lab_3/lab_3/src/test/java/com/lab3/model/dao/CommentDAOTest.java
@@ -10,6 +10,7 @@ import com.lab3.model.entity.Game;
 import com.lab3.model.entity.Rating;
 import com.lab3.model.entity.Users;
 import java.sql.Timestamp;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -49,28 +50,63 @@ public class CommentDAOTest {
     
     @Inject
     private UserTransaction tx;  //transaction needed for testing
-
+   
     @Test
-    public void create_comment() throws Exception{
+	public void createCommentAstract() throws Exception{
         //starts transaction
         tx.begin();
-        
+
         //create entities
         Users user5 = new Users("mail5", "name5", "password5");
         Game game5 = new Game("Game5");
         Comment comment1 = new Comment(user5,game5,"comment_text1",new Timestamp(System.currentTimeMillis()));
         Comment comment2 = new Comment(user5,game5,"comment_text2",new Timestamp(System.currentTimeMillis()));
-        
+
         usersDAO.create(user5);
         gameDAO.create(game5);
         commentDAO.create(comment1);
         commentDAO.create(comment2);
+        
+        //flush after create
+        usersDAO.getEntityManager().flush();
+        gameDAO.getEntityManager().flush();
+        commentDAO.getEntityManager().flush();
+
+        Assert.assertTrue(commentDAO.findAll().size() == 2);
+        
+        //refresh before delete
+        usersDAO.getEntityManager().refresh(user5);
+        gameDAO.getEntityManager().refresh(game5);
+        commentDAO.getEntityManager().refresh(comment1);
+        commentDAO.getEntityManager().refresh(comment2);
+
+        commentDAO.remove(comment1);
+        commentDAO.remove(comment2);
+        gameDAO.remove(game5);
+        usersDAO.remove(user5); 
+        
+        //ends the transaction
+        tx.commit();
+	}
+    
+    @Test
+	public void createComment() throws Exception{
+        //starts transaction
+        tx.begin();
+        Users user5 = new Users("mail5", "name5", "password5");
+        Game game5 = new Game("Game5");
+        usersDAO.create(user5);
+        gameDAO.create(game5);
+        
+        Comment comment1 = commentDAO.createComment(game5, user5, "commentText1");
+        Comment comment2 = commentDAO.createComment(game5, user5, "commentText2");
+        
         //flush after create
         usersDAO.getEntityManager().flush();
         gameDAO.getEntityManager().flush();
         commentDAO.getEntityManager().flush();
         
-        System.out.println("Query Result commentDAOtest findCommentsWithUsername: " + commentDAO.findCommentsWithUsername("mail5"));
+        Assert.assertTrue(commentDAO.findAll().size() == 2);
         
         //refresh before delete
         usersDAO.getEntityManager().refresh(user5);
@@ -78,12 +114,127 @@ public class CommentDAOTest {
         commentDAO.getEntityManager().refresh(comment1);
         commentDAO.getEntityManager().refresh(comment2);
         
-        //delete game and user comment should be removed by cascade
+        commentDAO.remove(comment1);
+        commentDAO.remove(comment2);
         gameDAO.remove(game5);
-        usersDAO.remove(user5);
-        Assert.assertTrue(true);
+        usersDAO.remove(user5);  
         
         //ends the transaction
         tx.commit();
-    }
+	}
+
+//  Test Fails    
+//    @Test
+//	public void create_comment_fails() throws Exception{
+//        //starts transaction
+//        tx.begin();
+//        
+//        Users user5 = new Users("mail5", "name5", "password5");
+//        Game game5 = new Game("Game5");
+//        gameDAO.create(game5);
+//        
+//        Comment comment1 = commentDAO.createComment(game5, user5, "commentText1");
+//        Comment comment2 = commentDAO.createComment(game5, user5, "commentText2");
+//        
+//        //flush after create
+//        usersDAO.getEntityManager().flush();
+//        gameDAO.getEntityManager().flush();
+//        commentDAO.getEntityManager().flush();
+//
+//        Assert.assertNull(comment1);
+//        Assert.assertNull(comment2);
+//        Assert.assertTrue(commentDAO.findAll().isEmpty());
+//        
+//        //refresh before delete
+//        gameDAO.getEntityManager().refresh(game5);
+//        commentDAO.getEntityManager().refresh(comment1);
+//        commentDAO.getEntityManager().refresh(comment2);
+//        
+//        commentDAO.remove(comment1);
+//        commentDAO.remove(comment2);
+//        usersDAO.remove(user5); 
+//        
+//        //ends the transaction
+//        tx.commit();
+//	}
+    
+    @Test
+	public void find_comments_with_usermail() throws Exception{
+        //starts transaction
+        tx.begin();
+        
+        Users user5 = new Users("mail5", "name5", "password5");
+        Game game5 = new Game("Game5");
+        
+        Comment comment1 = commentDAO.createComment(game5, user5, "commentText1");
+        Comment comment2 = commentDAO.createComment(game5, user5, "commentText2");
+               
+        usersDAO.create(user5);
+        gameDAO.create(game5);
+        commentDAO.create(comment1);
+        commentDAO.create(comment2);
+        
+        //flush after create
+        usersDAO.getEntityManager().flush();
+        gameDAO.getEntityManager().flush();
+        commentDAO.getEntityManager().flush();
+        
+        List list = commentDAO.findCommentsWithUsermail(user5.getMail());
+        Assert.assertTrue(list.get(0).equals(comment1));
+        Assert.assertTrue(list.get(1).equals(comment2));
+        
+        //refresh before delete
+        usersDAO.getEntityManager().refresh(user5);
+        gameDAO.getEntityManager().refresh(game5);
+        commentDAO.getEntityManager().refresh(comment1);
+        commentDAO.getEntityManager().refresh(comment2);
+        
+        commentDAO.remove(comment1);
+        commentDAO.remove(comment2);
+        gameDAO.remove(game5);
+        usersDAO.remove(user5); 
+        
+        //ends the transaction
+        tx.commit();
+	}
+    
+    @Test
+	public void find_comments_with_game() throws Exception{
+        //starts transaction
+        tx.begin();
+        
+        Users user5 = new Users("mail5", "name5", "password5");
+        Game game5 = new Game("Game5");
+        
+        Comment comment1 = commentDAO.createComment(game5, user5, "commentText1");
+        Comment comment2 = commentDAO.createComment(game5, user5, "commentText2");
+               
+        usersDAO.create(user5);
+        gameDAO.create(game5);
+        commentDAO.create(comment1);
+        commentDAO.create(comment2);
+        
+        //flush after create
+        usersDAO.getEntityManager().flush();
+        gameDAO.getEntityManager().flush();
+        commentDAO.getEntityManager().flush();
+        
+        List list = commentDAO.findCommentsWithGame(game5);
+        Assert.assertTrue(list.get(0).equals(comment1));
+        Assert.assertTrue(list.get(1).equals(comment2));
+        
+        //refresh before delete
+        usersDAO.getEntityManager().refresh(user5);
+        gameDAO.getEntityManager().refresh(game5);
+        commentDAO.getEntityManager().refresh(comment1);
+        commentDAO.getEntityManager().refresh(comment2);
+        
+        commentDAO.remove(comment1);
+        commentDAO.remove(comment2);
+        gameDAO.remove(game5);
+        usersDAO.remove(user5); 
+        
+        //ends the transaction
+        tx.commit();
+	}
 }
