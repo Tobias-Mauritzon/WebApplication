6af58481bfa44fd025bb51ba7b93338c21a609d0
Lerbyn <lerbyn98@gmail.com>
2021-02-18 09:05:58 +0100

    Lab3 renamed to gamesite and moved to assignment 4 folder
    removed the lab3 projects that didn't work to speed up delete and move operations

--- Lab_3/Assignment3_python/Assignment3_python/gamesite/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GamesiteConfig(AppConfig):
-    name = 'gamesite'
---
+++ Lab_4/Gamesite/src/main/java/com/gamesite/model/dao/AbstractDAO.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab3.model.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import lombok.RequiredArgsConstructor;
+
+/**
+ *
+ * @author Matteus
+ * @param <T>
+ */
+
+@RequiredArgsConstructor
+public abstract class AbstractDAO<K,T> {final Class<T> entityType;
+    protected abstract EntityManager getEntityManager();
+    public long count() {
+        final CriteriaBuilder builder = getEntityManager().getCriteriaBuilder();
+        final CriteriaQuery cq = builder.createQuery();
+        final Root<T> rt = cq.from(entityType);
+        
+        cq.select(builder.count(rt));
+        
+        final Query q = getEntityManager().createQuery(cq);
+        return ((Long) q.getSingleResult());
+    }
+    public void create(T entity
+
+        ) {getEntityManager().persist(entity);
+    }
+
+    public List<T> findAll() {
+        final CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityType));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+
+    public void remove(T entity) {
+        getEntityManager().remove(entity);
+    }
+    
+    public T find(K key) {
+        return getEntityManager().find(entityType, key);
+    }
+    
+    public boolean excist(K key) {
+        return getEntityManager().find(entityType, key) != null;
+    }
+}
+
