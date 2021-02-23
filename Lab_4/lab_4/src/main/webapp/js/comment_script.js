af5338d432b5d790ea696c30f69c7a4ff4cc1fd4
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-02-23 11:29:03 +0100

    Added frontend functions and inserts for comment submittions

---
+++ Lab_4/lab_4/src/main/webapp/js/comment_script.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function ratingSubmit(){
+    console.log(document.getElementById('game-type').name);
+    return document.getElementById('game-type').name;
+}
