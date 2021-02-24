7e70dca0280975a69e4c1595697d9d4d4748fe00
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-02-24 14:44:47 +0100

    Create db_init.sql
    Run these to add games to db

---
+++ Lab_4/lab_4/db_init.sql
@@ -0,0 +1,6 @@
+INSERT INTO game VALUES  'chat';
+INSERT INTO game VALUES  'rotation';
+INSERT INTO game VALUES  'addition';
+INSERT INTO game VALUES  'glider';
+INSERT INTO game VALUES  'matchstick';
+INSERT INTO game VALUES  'runner';
