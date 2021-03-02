3a4bd4fb0b5ec50fa539b7df8c5160780076e3ab
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-03-02 17:52:52 +0100

    Fixed merge problem added removed code

--- Project/src/main/java/com/lab3/controller/CommentController.java
+++ Project/src/main/java/com/lab3/controller/CommentController.java
@@ -25,82 +25,103 @@ import org.primefaces.PrimeFaces;
  */
 @RequestScoped
 @Named
-public class CommentController implements Serializable{
-    
+public class CommentController implements Serializable {
+
     @EJB
     private CommentDAO commentDAO;
-    
+
     @EJB
     private GameDAO gameDAO;
-    
+
     @EJB
     private UserAccountDAO userAccountDAO;
-    
+
     @Inject
     private CommentView commentView;
-    
-    /**Creates a comment for the given user on the game page
-     * 
+
+    /**
+     * Creates a comment for the given user on the game page
+     *
      * @param userName
      * @return false if rating could not be created otherwise true
      */
-    public boolean create(String userName){
+    public boolean create(String userName) {
         boolean res = true;
-        try{    
-            //Hard coded user should be removed
-            commentDAO.createComment(gameDAO.findGameMatchingName(commentView.getGameName()), userAccountDAO.findUsersWithName(userName), commentView.getText());
-            Messages.addGlobalInfo("Comment created");
-        }catch(Exception e){
-            res = false;
-            Messages.addGlobalError("Comment could not be created");
+        boolean signedIn = true;
+        boolean gameFound = true;
+
+        UserAccount user = new UserAccount("sdf", "ssefadf", "user", "asdfasfe");
+        Game game = new Game("saef", "afasdgf", "testPath");
+
+        try {
+            user = userAccountDAO.findUsersWithName(userName);
+        } catch (Exception e) {
+            signedIn = false;
+            Messages.addGlobalError("User not found or logged in");
         }
-        
+
+        try {
+            game = gameDAO.findGameMatchingName(commentView.getGameName());
+        } catch (Exception e) {
+            gameFound = false;
+            Messages.addGlobalError("Game not found");
+        }
+
+        if (signedIn && gameFound) {
+            try {
+                commentDAO.createComment(game, user, commentView.getText());
+                Messages.addGlobalInfo("Comment created");
+            } catch (Exception e) {
+                res = false;
+                Messages.addGlobalError("Comment could not be created");
+            }
+        }
+
         return res;
     }
-    
+
     /**
      * Finds all comments for the current game in a descending order
      */
-    public void findComments(){
-        try{
-            
-            if(commentView.getDescending()){
+    public void findComments() {
+        try {
+
+            if (commentView.getDescending()) {
                 List<Comment> list = commentDAO.findCommentsWithGamenameASC(commentView.getGameName());
                 commentView.setCommentList(list);
-            }
-            else{
+            } else {
                 List<Comment> list = commentDAO.findCommentsWithGamenameDESC(commentView.getGameName());
                 commentView.setCommentList(list);
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             Messages.addGlobalError("No comments");
         }
     }
-    
+
     /**
-     * Flips the boolean flag that dictates which direction the comments are rendered in
+     * Flips the boolean flag that dictates which direction the comments are
+     * rendered in
      */
-    public void flipDescending(){
-            if(commentView.getDescending()){
-                commentView.setDescending(Boolean.FALSE);
-            }
-            else{
-                commentView.setDescending(Boolean.TRUE);
-            }
+    public void flipDescending() {
+        if (commentView.getDescending()) {
+            commentView.setDescending(Boolean.FALSE);
+        } else {
+            commentView.setDescending(Boolean.TRUE);
+        }
     }
-    
-    /**Finds and sets the game entity with the given gameName
-     * 
+
+    /**
+     * Finds and sets the game entity with the given gameName
+     *
      */
-    public void findGame(){
-        try{
+    public void findGame() {
+        try {
             Game game = gameDAO.findGameMatchingName(commentView.getGameName());
             commentView.setGame(game);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             Messages.addGlobalError("Can't find game");
         }
     }
-    
-    
+
 }
