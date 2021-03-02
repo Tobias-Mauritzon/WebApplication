6da65d2e73f2e1d3f5959cec972ff3bc23bf24b3
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-03-02 14:48:50 +0100

    Update CommentController.java
    CommentController have more responses to submits

--- Project/src/main/java/com/lab3/controller/CommentController.java
+++ Project/src/main/java/com/lab3/controller/CommentController.java
@@ -10,6 +10,7 @@ import com.lab3.model.dao.GameDAO;
 import com.lab3.model.dao.UserAccountDAO;
 import com.lab3.model.entity.Comment;
 import com.lab3.model.entity.Game;
+import com.lab3.model.entity.Rating;
 import com.lab3.model.entity.UserAccount;
 import com.lab3.view.CommentView;
 import java.io.Serializable;
@@ -29,64 +30,88 @@ import org.omnifaces.util.Messages;
  */
 @RequestScoped
 @Named
-public class CommentController implements Serializable{
-    
+public class CommentController implements Serializable {
+
     @EJB
     private CommentDAO commentDAO;
-    
+
     @EJB
     private GameDAO gameDAO;
-    
+
     @EJB
     private UserAccountDAO userAccountDAO;
-    
+
     @Inject
     private CommentView commentView;
-    
-    /**Creates a comment for the given user on the game page
-     * 
+
+    /**
+     * Creates a comment for the given user on the game page
+     *
      * @param userName
      * @return false if rating could not be created otherwise true
      */
-    public boolean create(String userName){
+    public boolean create(String userName) {
         boolean res = true;
-        try{    
-            //Hard coded user should be removed
-            commentDAO.createComment(gameDAO.findGameMatchingName(commentView.getGameName()), userAccountDAO.findUsersWithName(userName), commentView.getText());
-            Messages.addGlobalInfo("Comment created");
-        }catch(Exception e){
-            res = false;
-            Messages.addGlobalError("Comment could not be created");
+
+        boolean signedIn = true;
+        boolean gameFound = true;
+
+        UserAccount user = new UserAccount("sdf", "ssefadf", "user", "asdfasfe");
+        Game game = new Game("saef", "afasdgf", "testPath");
+
+        try {
+            user = userAccountDAO.findUsersWithName(userName);
+        } catch (Exception e) {
+            signedIn = false;
+            Messages.addGlobalError("User not found or logged in");
         }
-        
+
+        try {
+            game = gameDAO.findGameMatchingName(commentView.getGameName());
+        } catch (Exception e) {
+            gameFound = false;
+            Messages.addGlobalError("Game not found");
+        }
+
+        if (signedIn && gameFound) {
+            try {
+                commentDAO.createComment(game, user, commentView.getText());
+                Messages.addGlobalInfo("Comment created");
+            } catch (Exception e) {
+                res = false;
+                Messages.addGlobalError("Comment could not be created");
+            }
+        }
+
         return res;
     }
-    
-    /**Finds all comments for the current game
-     * 
+
+    /**
+     * Finds all comments for the current game
+     *
      */
-    public void findComments(){
-        try{
+    public void findComments() {
+        try {
             List<Comment> list = commentDAO.findCommentsWithGamenameDESC(commentView.getGameName());
             commentView.setCommentList(list);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             Messages.addGlobalError("No comments");
         }
     }
-    
-    /**Finds and sets the game entity with the given gameName
-     * 
+
+    /**
+     * Finds and sets the game entity with the given gameName
+     *
      */
-    public void findGame(){
-        try{
+    public void findGame() {
+        try {
             Game game = gameDAO.findGameMatchingName(commentView.getGameName());
             commentView.setGame(game);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             Messages.addGlobalError("Can't find game");
         }
     }
-    
-    
+
 }
