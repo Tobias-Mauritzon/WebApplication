be969338f3133556a71b5b97ed63d0217ff743e6
Game9954 <60510013+Game9954@users.noreply.github.com>
2021-03-04 15:07:29 +0100

    New db init, you have to run this for db to work

--- Project/db_init.sql
+++ Project/db_init.sql
@@ -1,10 +1,10 @@
 /*Run the application once before you run this sql file*/
 
-INSERT INTO game (name, description, javascript) VALUES  ('chat','chat game a very good game where you can chat with yourself', 'js/chat_script.js');
-INSERT INTO game (name, description, javascript) VALUES  ('rotation', 'rotation game a very average game', 'js/rotation_script.js');
-INSERT INTO game (name, description, javascript) VALUES  ('addition', 'Here you can experience addition like you never have before', 'js/addition_script.js');
-INSERT INTO game (name, description, javascript) VALUES  ('glider', 'Glide like you have never glidden before', 'js/glider_script.js');
-INSERT INTO game (name, description, javascript) VALUES  ('matchstick', 'Outsmart the AI in this high octane game', 'js/matchstick_script.js');
-INSERT INTO game (name, description, javascript) VALUES  ('runner', 'Orkar ej', 'js/runner_script.js');
+INSERT INTO game (name, author, description, javascript, image, tstamp) VALUES  ('Chat','Matteus', 'A game where you can chat in a chatbox with yourself.', 'js/chat_script.js','Resources/chat_image.png',CURRENT_TIMESTAMP);
+INSERT INTO game (name, author, description, javascript, image, tstamp) VALUES  ('Rotation','Matteus', 'A game where you rotate a cube by clicking a button.', 'js/rotation_script.js','Resources/rotation_image.png',CURRENT_TIMESTAMP);
+INSERT INTO game (name, author, description, javascript, image, tstamp) VALUES  ('Addition','Matteus', 'A game where you add to a number by 1.', 'js/addition_script.js','Resources/addition_image.png',CURRENT_TIMESTAMP);
+INSERT INTO game (name, author, description, javascript, image, tstamp) VALUES  ('Glider','Tobias', 'Glide like you have never glidden before', 'js/glider_script.js','Resources/glider_image.png',CURRENT_TIMESTAMP);
+INSERT INTO game (name, author, description, javascript, image, tstamp) VALUES  ('Matchstick','Joachim', 'Outsmart the AI in this high octane game', 'js/matchstick_script.js','Resources/matchstick_image.png',CURRENT_TIMESTAMP);
+INSERT INTO game (name, author, description, javascript, image, tstamp) VALUES  ('Runner','Matteus', 'A game where you jump over boxes as a squirrel', 'js/runner_script.js','Resources/runner_image.png',CURRENT_TIMESTAMP);
 
 INSERT INTO useraccount (mail, name, password, usergroup) VALUES  ('admin@mail.se', 'admin', 'PBKDF2WithHmacSHA256:2048:mTvEEovtLyI7wwZp1iOhufxAF0lydA2sX5V+2OOZMIk=:lqRGqAdnW9mU3fxkJXh92AgOp1qxWIwSKPEhV4AoIgY=','ADMIN');
