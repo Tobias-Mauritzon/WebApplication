a82f8be173010dc98deee44182fedf67af611098
Tobias-Mauritzon <tobmau@student.chalmers.se>
2021-03-05 11:38:24 +0100

    Background change is more consistent

--- Project/src/main/webapp/js/glider_script.js
+++ Project/src/main/webapp/js/glider_script.js
@@ -2,6 +2,8 @@
 var canvas;
 var ctx;
 var background_image;
+var background_image_heigth;
+var background_image_width;
 var pawn_canvas;
 var pawn_ctx;
 var pawn;
@@ -10,128 +12,133 @@ var pawn_pos_y;
 var speed = 5;
 var map = {};
 
-$(document).ready(function(){
-       
-    $("#start-button").click(function(){
-        
+$(document).ready(function () {
+
+    $("#start-button").click(function () {
+
         // Initializing canvas
         canvas = document.getElementById('game-canvas');
         ctx = canvas.getContext('2d');
-        background_image = new Image(1000, 700);
+        background_image_heigth = canvas.height;
+        background_image_width = canvas.width;
+        console.log(background_image_width);
+        console.log(background_image_heigth);
+        background_image = new Image(background_image_width, background_image_heigth);
         background_image.src = "Resources/glider_res/terrang4.png";
         spawn_background();
-        
+
         // Add keyboard events for movement
         document.addEventListener('keydown', (event) => {
             map[event.which] = true;
         });
-        
+
         document.addEventListener('keyup', (event) => {
             map[event.which] = false;
         });
-        
-        
+
+
         document.addEventListener('keypress', (event) => {
             key_press();
         });
-         
-        
-        
+
+
+
         //Craeting and spawning the pawn in the canvas
         pawn_ctx = canvas.getContext('2d');
-        
+
         pawn_image = new Image(64, 64);
         pawn_image.src = "Resources/glider_res/gubbe2.png";
-        spawn_pawn();        
-         
-    });    
-  
-  }); 
+        spawn_pawn();
 
-function spawn_background(){
-    background_image.onload = function() {
+    });
+
+});
+
+function spawn_background() {
+    background_image.onload = function () {
         ctx.drawImage(background_image, 0, 0);
-    } 
+    }
 }
-function spawn_pawn(){
-    pawn_image.onload = function() {
+function spawn_pawn() {
+    pawn_image.onload = function () {
         pawn_pos_x = 0;
         pawn_pos_y = 0;
         pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y);
-        
-    } 
+
+    }
 }
-function key_press(){
+function key_press() {
 
     //A
-    if(map[65]){
+    if (map[65]) {
         pawn_pos_x += -speed;
         change_backround();
     }
 
     //W
-    if(map[87]){
+    if (map[87]) {
         pawn_pos_y += -speed;
         change_backround();
     }
 
     //D
-    if(map[68]){
+    if (map[68]) {
         pawn_pos_x += speed;
         change_backround();
     }
 
     //S
-    if(map[83]){
+    if (map[83]) {
         pawn_pos_y += speed;
 
         change_backround();
     }
 
     //R
-    if(map[82]){
+    if (map[82]) {
         pawn_pos_x = 0;
         pawn_pos_y = 0;
     }
 
     // I
-    if(map[73]){
+    if (map[73]) {
         speed += 5;
     }
 
     // U
-    if(map[85]){
+    if (map[85]) {
         speed += -5;
     }
 
     ctx.drawImage(background_image, 0, 0);
-    pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y); 
+    pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y);
 }
 
-function re_draw_background(){
+function re_draw_background() {
     var map = Math.floor(Math.random() * 4) + 1;
-    background_image.src = "Resources/glider_res/terrang"+map+".png";
+    background_image.src = "Resources/glider_res/terrang" + map + ".png";
     ctx.drawImage(background_image, 0, 0);
+    pawn_ctx.drawImage(pawn_image, pawn_pos_x, pawn_pos_y);
 }
 
-function change_backround(){
-    if(pawn_pos_x + 64 > 1000){
-        re_draw_background();
-        pawn_pos_x += -1000;
+function change_backround() {
+    if (pawn_pos_x > background_image_width) {
+        pawn_pos_x += -background_image_width;
+        re_draw_background();    
     }
-    
-    if(pawn_pos_x + 64 < 0){
-        re_draw_background();
-        pawn_pos_x += 1000;
+
+    if (pawn_pos_x + 64 < 0) {
+        pawn_pos_x += background_image_width;
+        re_draw_background();      
     }
-    
-    if(pawn_pos_y + 64 > 700){
+
+    if (pawn_pos_y > background_image_heigth) {
+        pawn_pos_y += -background_image_heigth;
         re_draw_background();
-        pawn_pos_y += -700;
     }
-    
-    if(pawn_pos_y + 64 < 0){
+
+    if (pawn_pos_y + 64 < 0) {
+        pawn_pos_y += background_image_heigth;
         re_draw_background();
-        pawn_pos_y += 700;
     }
 }
