cba54b6198694af4bb0ddcbb0a9c57d488d2627d
Game9954 <60510013+Game9954@users.noreply.github.com>
2021-03-05 11:40:15 +0100

    Update runner to disable start button after one click and stop sceen scrolling when clicking space

--- Project/src/main/webapp/js/runner_script.js
+++ Project/src/main/webapp/js/runner_script.js
@@ -8,114 +8,119 @@ var player;
 var obstacle;
 var background;
 
-$(document).ready(function(){
-  canvas = document.getElementById("game-canvas");
-  player = new Player();
-  obstacle = new Obstacle();
-  background = new Background();
+document.defaultView.onkeydown = function(e) {
+    return e.keyCode !== 32;
+};
 
-  $("#start-button").click(function(){
+$(document).ready(function () {
+    canvas = document.getElementById("game-canvas");
+    player = new Player();
+    obstacle = new Obstacle();
+    background = new Background();
 
-    ctx = canvas.getContext("2d");
+    $("#start-button").click(function () {
+        $("#start-button").addClass("disabled").prop("disabled", true);
+        ctx = canvas.getContext("2d");
 
-    document.addEventListener('keydown', (e) => {
-      var key = e.which;
-      if(key == 32)  // the space key code
-       {
-        player.jump();
-       }
-    })
+        document.addEventListener('keydown', (e) => {
+            var key = e.which;
+            if (key == 32)  // the space key code
+            {
+                player.jump();
+            }
+            return false;
+        })
 
-    setInterval(onTimerTick, 33); // 33 milliseconds = ~ 30 frames per sec
+        setInterval(onTimerTick, 33); // 33 milliseconds = ~ 30 frames per sec
 
-    function onTimerTick() {
-        player.update();
-        obstacle.update();
+        function onTimerTick() {
+            player.update();
+            obstacle.update();
 
-        background.draw();
-        player.draw();
-        obstacle.draw();
-    }
-  });
+            background.draw();
+            player.draw();
+            obstacle.draw();
+        }   
+    });
 });
 
-function Vector(x,y,dx,dy){
-  // position
-  this.x = x || 0;
-  this.y = y || 0;
-  // direction
-  this.dx = dx || 0;
-  this.dy = dy || 0;
+function Vector(x, y, dx, dy) {
+    // position
+    this.x = x || 0;
+    this.y = y || 0;
+    // direction
+    this.dx = dx || 0;
+    this.dy = dy || 0;
 }
 
 /**
  * Advance the vectors position by dx,dy
  */
-Vector.prototype.advance = function() {
+Vector.prototype.advance = function () {
     this.x += this.dx;
     this.y += this.dy;
-  };
+};
 
 
 function Player() {
-  this.width = 100;
-  this.height = 100;
-  this.gravity = 1;
-  this.jumpVelocity = 20;
-  this.image = new Image(this.width,this.height);
-  this.image.src = "Resources/squirrel.png";
-  this.vector = new Vector(200,600,0,0);
+    this.width = 100;
+    this.height = 100;
+    this.gravity = 1;
+    this.jumpVelocity = 20;
+    this.image = new Image(this.width, this.height);
+    this.image.src = "Resources/squirrel.png";
+    this.vector = new Vector(200, 600, 0, 0);
 }
 
-Player.prototype.update = function(){
-  if(this.vector.y < 700 -this.height){
-    this.vector.dy += this.gravity;
-  }
-  if(this.vector.y > (700 -this.height)){
-    this.vector.dy = 0;
-    this.vector.y = 700 -this.height;
-  }
+Player.prototype.update = function () {
+    if (this.vector.y < 700 - this.height) {
+        this.vector.dy += this.gravity;
+    }
+    if (this.vector.y > (700 - this.height)) {
+        this.vector.dy = 0;
+        this.vector.y = 700 - this.height;
+    }
 
-  this.vector.advance();
+    this.vector.advance();
 
 }
 
-Player.prototype.jump = function(){
-  if(this.vector.dy == 0){
-    this.vector.dy = -this.jumpVelocity;
-  }
+Player.prototype.jump = function () {
+    if (this.vector.dy == 0) {
+        this.vector.dy = -this.jumpVelocity;
+    }
 }
 
-Player.prototype.draw = function(){
-  ctx.drawImage(this.image,this.vector.x,this.vector.y,this.width,this.height);
+Player.prototype.draw = function () {
+    ctx.drawImage(this.image, this.vector.x, this.vector.y, this.width, this.height);
 }
 
 
 function Obstacle() {
-  this.width = 80;
-  this.height = 80;
-  this.image = new Image(this.width,this.height);
-  this.image.src = "Resources/matchstick/table.png";
-  this.vector = new Vector(700,700-this.height,-10,0);
+    this.width = 80;
+    this.height = 80;
+    this.image = new Image(this.width, this.height);
+    this.image.src = "Resources/matchstick/table.png";
+    this.vector = new Vector(700, 700 - this.height, -10, 0);
 }
 
-Obstacle.prototype.update = function(){
-  if(this.vector.x <= 0){
-    this.vector.x = 700
-  }
-  this.vector.advance();
+Obstacle.prototype.update = function () {
+    if (this.vector.x <= 0) {
+        this.vector.x = 700
+    }
+    this.vector.advance();
 }
 
-Obstacle.prototype.draw = function(){
-  ctx.drawImage(this.image,this.vector.x,this.vector.y,this.width,this.height);
+Obstacle.prototype.draw = function () {
+    ctx.drawImage(this.image, this.vector.x, this.vector.y, this.width, this.height);
 }
 
 
 function Background() {
-  this.image = new Image(this.width,this.height);
-  this.image.src = "Resources/windows.jpg";
+    this.image = new Image(this.width, this.height);
+    this.image.src = "Resources/windows.jpg";
 }
 
-Background.prototype.draw = function(){
-  ctx.drawImage(this.image,0,0);
+Background.prototype.draw = function () {
+    ctx.drawImage(this.image, 0, 0);
 }
