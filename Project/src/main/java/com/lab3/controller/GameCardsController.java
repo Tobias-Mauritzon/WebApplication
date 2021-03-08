64b257b6905e268b1f10a28fcdf3213d74cf677b
Lerbyn <lerbyn98@gmail.com>
2021-03-08 08:29:42 +0100

    game switching works from game cards and load dynamically

--- Project/src/main/java/com/lab3/controller/GameCardsController.java
+++ Project/src/main/java/com/lab3/controller/GameCardsController.java
@@ -2,11 +2,14 @@ package com.lab3.controller;
 
 import com.lab3.model.dao.GameDAO;
 import com.lab3.model.entity.Game;
+import com.lab3.view.CurrentGameView;
 import com.lab3.view.GameCardsView;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.omnifaces.util.Messages;
@@ -25,6 +28,9 @@ public class GameCardsController implements Serializable{
     @Inject
     private GameCardsView gameCardsView;
     
+    @Inject 
+    private CurrentGameView currentGameView;
+    
     /**Finds all games in the database and adds them to a 
      * list of games
      * 
@@ -39,5 +45,11 @@ public class GameCardsController implements Serializable{
         }catch(Exception e){
             Messages.addGlobalError("No Games");
         }
+    }
+    
+    public void setGameAndRedirect(String game) throws IOException {
+        currentGameView.setGame(game);
+        System.out.println("game set to " + game);
+        FacesContext.getCurrentInstance().getExternalContext().redirect("game.xhtml");
     }
 }
