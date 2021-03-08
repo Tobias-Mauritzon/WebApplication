045bc45d1f32aac076fddd9b956618246a3026f1
Joachim <60391253+joantfolk@users.noreply.github.com>
2021-03-08 13:07:38 +0100

    Revert "Update to matchstick game."
    This reverts commit 5f2eb9476e0970b30d090f53e2c57cc152f68225.

--- Project/src/main/webapp/js/matchstick_script.js
+++ Project/src/main/webapp/js/matchstick_script.js
@@ -4,120 +4,68 @@ var ctx;
 var background_image;
 var match;
 var matches;
-var turn;
 
 $(document).ready(function(){
     
     // Initializing
     canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext("2d");
-    turn = true;
     background_image = new Image(1000, 600);
     background_image.src = "Resources/matchstick/table.png";
     ctx.drawImage(background_image, 0, 0);
+
     reset();
     spawnBackground();
     
     $("#removeOne").click(function(){
-        if(turn){
-            playerMove(1);
-            setTimeout(() => {computerMove();}, 1000);
-        }
+       removeOne();
+       Redraw();
     });
     
     $("#removeTwo").click(function(){
-        if(turn){
-            playerMove(2);
-            setTimeout(() => {computerMove();}, 1000);
-        }
+        removeTwo();
+        Redraw();
     });
     
     $("#reset").click(function(){
         reset();
-        redraw();
+        Redraw();
     });
 });
 
 function spawnBackground(){
     background_image.onload = function() {
-        Redraw();
+        ctx.drawImage(background_image, 0, 0);    
+        for(i = 0; i < count; i++){
+            ctx.drawImage(matches[i].image, matches[i].x, matches[i].y);
+        }
     };
 }
 
-function redraw(){
+function Redraw(){
     ctx.drawImage(background_image, 0, 0);    
     for(i = 0; i < count; i++){
         ctx.drawImage(matches[i].image, matches[i].x, matches[i].y);
     }
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "black";
-    ctx.textAlign = "left";
-    ctx.fillText("Matches left: " + count, 30, 30);
 }
 
-function remove(number){
-    if(count > 1){
-        count -= number;
-    } else {
-        count -= 1;
-    }
+function removeOne(){
+    count -= 1;
 }
 
-function checkWin(){
-    if(count <= 0){
-        if(turn){
-            printMid("YOU WIN!");
-        } else {
-            printMid("YOU LOSE!");
-        }
-    }
-}
-
-function printMid(str){
-    ctx.font = "50px Arial";
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
-    ctx.fillText(str, canvas.width/2, canvas.height/2);
+function removeTwo(){
+    count -= 2;
 }
 
 function reset(){
     count = 0;
     matches = [];
-	turn = true;
     for(i = 0; i < 21; i++){
         match = new Image(40, 140);
-        match.src = "Resources/matchstick/matchstick (" + parseInt(1 + Math.random() * 7) + ").png";
-        var xPos = 100 + Math.random() * 700;
-        var yPos = 100 + Math.random() * 300;
+        match.src = "Resources/matchstick/matchstick.png";
+        var xPos = Math.random() * 900;
+        var yPos = Math.random() * 450;
         matches.push({image:match, x: xPos, y:yPos});
     }
     count = 21;
 }
-
-function playerMove(number){
-    if(count > 0){
-        remove(number);
-        turn = false;
-        redraw();
-    }
-    checkWin();
-}
-
-function computerMove(){
-    if(count > 0){
-        if(((count % 3) === 0) || (((count + 1) % 3) === 0)){ //Check winning positionS
-            if((count % 3) === 0){
-                remove(2);
-            }
-            else{
-                remove(1);
-            }
-        } else {
-            remove(1);
-        }
-        turn = true;
-        redraw();
-        checkWin();
-    }
-}
-
