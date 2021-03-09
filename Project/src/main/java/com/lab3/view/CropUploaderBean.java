a578b363b99701f324c4585454460e95d4a51b7f
Game9954 <60510013+Game9954@users.noreply.github.com>
2021-03-09 17:07:47 +0100

    Add image uploader and cropper to profile page
    These where taken from primfaces ImageCropper:
    https://www.primefaces.org/showcase/ui/multimedia/cropper/fileupload.xhtml?jfwid=746c5

---
+++ Project/src/main/java/com/lab3/view/CropUploaderBean.java
@@ -0,0 +1,97 @@
+package com.lab3.view;
+
+import java.io.ByteArrayInputStream;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.CroppedImage;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.file.UploadedFile;
+
+/**
+ *
+ * @author Matteus
+ */
+@Named
+@SessionScoped
+public class CropUploaderBean implements Serializable{
+
+    private CroppedImage croppedImage;
+
+    private UploadedFile originalImageFile;
+
+    public CroppedImage getCroppedImage() {
+        return croppedImage;
+    }
+
+    public void setCroppedImage(CroppedImage croppedImage) {
+        this.croppedImage = croppedImage;
+    }
+
+    public UploadedFile getOriginalImageFile() {
+        return originalImageFile;
+    }
+
+    public void handleFileUpload(FileUploadEvent event) {
+        this.originalImageFile = null;
+        this.croppedImage = null;
+        UploadedFile file = event.getFile();
+        if (file != null && file.getContent() != null && file.getContent().length > 0 && file.getFileName() != null) {
+            this.originalImageFile = file;
+            FacesMessage msg = new FacesMessage("Successful", this.originalImageFile.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    }
+
+    public void crop() {
+        if (this.croppedImage == null || this.croppedImage.getBytes() == null || this.croppedImage.getBytes().length == 0) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Cropping failed."));
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "Cropped successfully."));
+        }
+    }
+
+    public StreamedContent getImage() {
+        return DefaultStreamedContent.builder()
+                .contentType(originalImageFile == null ? null : originalImageFile.getContentType())
+                .stream(() -> {
+                    if (originalImageFile == null
+                            || originalImageFile.getContent() == null
+                            || originalImageFile.getContent().length == 0) {
+                        return null;
+                    }
+
+                    try {
+                        return new ByteArrayInputStream(originalImageFile.getContent());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .build();
+    }
+
+    public StreamedContent getCropped() {
+        return DefaultStreamedContent.builder()
+                .contentType(originalImageFile == null ? null : originalImageFile.getContentType())
+                .stream(() -> {
+                    if (croppedImage == null
+                            || croppedImage.getBytes() == null
+                            || croppedImage.getBytes().length == 0) {
+                        return null;
+                    }
+
+                    try {
+                        return new ByteArrayInputStream(this.croppedImage.getBytes());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .build();
+    }
+}
