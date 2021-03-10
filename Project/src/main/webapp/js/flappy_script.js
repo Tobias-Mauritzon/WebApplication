018738a88d21f43c379d92448b88ccfbf8427ad0
Simon <1mappat1@gmail.com>
2021-03-10 18:59:45 +0100

    colittion
    colittion and score work

--- Project/src/main/webapp/js/flappy_script.js
+++ Project/src/main/webapp/js/flappy_script.js
@@ -2,10 +2,15 @@ var canvas;
 var canvas_width = 700;
 var canvas_height = 700;
 var ctx;
+var bctx;
 var bird;
 var pipe1;
 var pipe2;
+var pipe21;
+var pipe22;
 var background;
+var score = [2]; //0 is current score, 1 is previous
+var birdDead;
 
 document.defaultView.onkeydown = function(e) {
     return e.keyCode !== 32;
@@ -14,9 +19,13 @@ document.defaultView.onkeydown = function(e) {
 $(document).ready(function () {
     canvas = document.getElementById("game-canvas");
     bird = new Bird();
-    pipe1 = new Pipe("Pipe");
-    pipe2 = new Pipe("PipeDown");
+    pipe1 = new Pipe("Pipe", 700);
+    pipe2 = new Pipe("PipeDown", 700);
+    pipe21 = new Pipe("Pipe", 1050);
+    pipe22 = new Pipe("PipeDown", 1050);
     background = new Background();
+    score[0] = 0;
+    birdDead = false;
 
     $("#start-button").click(function () {
         $("#start-button").addClass("disabled").prop("disabled", true);
@@ -37,12 +46,15 @@ $(document).ready(function () {
             bird.update();
             pipe2.updateTop(pipe1);
             pipe1.updateBottom();
+            pipe22.updateTop(pipe21);
+            pipe21.updateBottom();
             
             background.draw();
             bird.draw(); 
             pipe1.draw();
             pipe2.draw();
-           ;
+            pipe21.draw();
+            pipe22.draw();
         }   
     });
 });
@@ -59,6 +71,7 @@ function Bird() {
     this.image.src = "Resources/Bird/Birb-fly.png";
     this.vector = new Vector(270, 200, 0, 0);
     this.canJump;
+    this.angel = 180;
 }
 
 Bird.prototype.update = function () {
@@ -73,7 +86,6 @@ Bird.prototype.update = function () {
         this.canJump = true;
     }
     this.colittion();
-    
     this.vector.update();
 };
 
@@ -86,20 +98,39 @@ Bird.prototype.fly = function () {
 
 Bird.prototype.colittion = function () {
     front = this.vector.x+this.width;
+    
     if(front >= pipe1.vector.x && this.vector.x < (pipe1.vector.x + pipe1.width)){
-        if(this.vector.y > (pipe2.vector.y + pipe2.height)){ // bellow top pipe
-            console.log("bellow top");
-            if((this.vector.y + this.height) < pipe1.vector.y){ //above bottom pipe
-                console.log("above bottom");
-            }    
-            //Do nothing, you aren't hit
+        if(((this.vector.y >= pipe2.vector.y + pipe2.height)) && ((this.vector.y + this.height) <= pipe1.vector.y)){ //True if Bellow top pipe
+            //score
+            if(score[0] === score[1]){
+                score[0] = score[0] + 1;
+                console.log("SCORE");
+            }
         }
         else{
-            console.log("hit");
+            gameOver();
         }
-        
+    }
+    
+    
+    else if(front >= pipe21.vector.x && this.vector.x < (pipe21.vector.x + pipe21.width)){ //True if Bellow top pipe(same as above for the second instance of pipe)
+        if(((this.vector.y >= pipe22.vector.y + pipe22.height)) && ((this.vector.y + this.height) <= pipe21.vector.y)){ //True if Bellow top pipe
+            //score
+            if(score[0] === score[1]){
+                score[0] = score[0] + 1;
+                console.log("SCORE");
+            }
+        }
+        else{
+            gameOver();
+        }
+    }
+    else{
+        score[1] = score[0];
+        console.log("Reset for score");
     }
 };
+
 
 Bird.prototype.draw = function () {
     ctx.drawImage(this.image, this.vector.x, this.vector.y, this.width, this.height);
@@ -118,15 +149,15 @@ Vector.prototype.update = function () {
     this.y += this.dy;
 };
 
-function Pipe(pipeSprite) {
+function Pipe(pipeSprite, startXPos) {
     this.width = 75;
     this.height = 600;
     this.image = new Image(this.width, this.height);
     this.image.src = "Resources/Bird/" + pipeSprite + ".png";
     if(pipeSprite === "Pipe"){
-        this.vector = new Vector(700, 600 - (Math.random() * 400), -5, 0);    
+        this.vector = new Vector(startXPos, 600 - (Math.random() * 400), -10, 0);    
     }else{
-        this.vector = new Vector(700, 0, -5, 0);
+        this.vector = new Vector(startXPos, 0, -10, 0);
     }
     
 }
@@ -141,7 +172,7 @@ Pipe.prototype.updateTop = function (bottomPipe) {
 Pipe.prototype.updateBottom = function () {
     if (this.vector.x <= (-this.width)) {
         this.vector.x = 700;
-        this.vector.y = 600 - (Math.random() * 400);
+        this.vector.y = 650 - (Math.random() * 350);
     }
     this.vector.update();
 };
@@ -163,6 +194,14 @@ Background.prototype.draw = function () {
 };
 
 function gameOver(){
-    pipe1.vector.dy = 0;
-    pipe2.vector.dy = 0;
+    pipe1.vector.dx = 0;
+    pipe2.vector.dx = 0;
+    pipe21.vector.dx = 0;
+    pipe22.vector.dx = 0;
+    bird.flyStrength = 0;
+    if(birdDead === false){
+        bird.image.src = "Resources/Bird/Birb-sad.png";
+        console.log("lost with: " + score[0] + " points")
+    }
+    birdDead = true;
 }
