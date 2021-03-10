85c7bb3bef1ba0c73e6d59d0ce17afe8f5eaf8b5
Lerbyn <lerbyn98@gmail.com>
2021-03-10 16:32:28 +0100

    Games load dynamically from database in the drop down menu, Addition games works in iframe now

--- Project/src/main/java/com/lab3/view/CurrentGameView.java
+++ Project/src/main/java/com/lab3/view/CurrentGameView.java
@@ -6,7 +6,14 @@ import com.lab3.model.dao.UserAccountDAO;
 import com.lab3.model.entity.Game;
 import com.lab3.model.entity.UserAccount;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
 import javax.inject.Inject;
 import javax.inject.Named;
 import lombok.Data;
@@ -26,7 +33,17 @@ public class CurrentGameView implements Serializable{
     
     private String game;
     private Game gameObject;
+    private ArrayList games = new ArrayList();
     private UserAccount user;
+    
+    @PostConstruct
+    private void init() {
+        //load games on page load
+        List<Game> temp = gameDAO.findAllGames();
+        for(Game g: temp) {
+            games.add(new SelectItem(g.getName(),g.getName()));
+        }
+    }
     
     public void setUser(String user) {
         if(user != null) {
@@ -38,10 +55,5 @@ public class CurrentGameView implements Serializable{
         if(game != null) {
             this.gameObject = gameDAO.findGameMatchingName(game);        
         }
-    }
-    
-    public void setGame(String game) {
-        System.out.println("game set to " + game);
-        this.game = game;
     }
 }
