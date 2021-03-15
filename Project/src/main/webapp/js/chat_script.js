3baba56f060446279bdc8f166757dea6c1738a20
Game9954 <60510013+Game9954@users.noreply.github.com>
2021-03-15 15:20:07 +0100

    Update Chat game with score and script cleanup

--- Project/src/main/webapp/js/chat_script.js
+++ Project/src/main/webapp/js/chat_script.js
@@ -1,33 +1,44 @@
-$(document).ready(function(){
-
-    
-});
+// Author: Matteus
 
 var score;
 
+/**
+ * First time setup
+ */
 function loaded() {
-    score=0;
-    $('iframe[name=game-frame]').contents().find('#chatInput').keypress(function(e){
-        console.log("key pressed");
+    score = 0;
+
+    /**
+     * Key Press listner
+     */
+    $('iframe[name=game-frame]').contents().find('#chatInput').keypress(function (e) {
         var key = e.which;
         console.log(key);
-        if(key == 13)  // the enter key code
-         {
-            score = score + 1;
+
+        /**
+         * If enter is pressed add comment
+         */
+        if (key === 13)  // the enter key code
+        {
+            score++;
+            $('iframe[name=game-frame]').contents().find('#score-label').text("Score: " + score);
 
             var txt1 = $("<b></b>").text("you: ");
-            var txt2 = $("<p></p>").text($('iframe[name=game-frame]').contents().find('#chatInput').val()).css({"margin-bottom":"0","display":"inline"});
+            var txt2 = $("<p></p>").text($('iframe[name=game-frame]').contents().find('#chatInput').val()).css({"margin-bottom": "0", "display": "inline"});
 
             var div1 = $("<div></div>");
 
-            div1.append(txt1,txt2);
-            $('iframe[name=game-frame]').contents().find('#chat').append(div1);     
+            div1.append(txt1, txt2);
+            $('iframe[name=game-frame]').contents().find('#chat').append(div1);
             $('iframe[name=game-frame]').contents().find('#chatInput').val("");
-           return true;  
-         }
+            return true;
+        }
     });
-    
-    $('iframe[name=game-frame]').contents().find('#submit-score').click(function(){
-        setHighScore([{ name: "highscore", value: score}]);
+
+    /**
+     * Submit Score Button
+     */
+    $('iframe[name=game-frame]').contents().find('#submit-score').click(function () {
+        setHighScore([{name: "highscore", value: score}]);
     });
 }
