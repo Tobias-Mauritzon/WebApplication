6b1207b962cfa4889e2dc76af2238ff20b0d3e50
Game9954 <60510013+Game9954@users.noreply.github.com>
2021-03-15 15:26:47 +0100

    Update & fix games (#33)
    * Move Flappy Game to games folder and change paths
    
    * Update Addition game with score and script cleanup
    
    * Update Chat game with score and script cleanup
    
    * Update Rotation game with score and script cleanup
    
    * Update Runner game with score and script cleanup
    
    * Update Glider html with score and script cleanup
    
    * Fix matchstick game import paths
    
    * Cleanup addition game

--- Project/src/main/webapp/js/addition_script.js
+++ Project/src/main/webapp/js/addition_script.js
@@ -1,14 +1,24 @@
+// Author: Matteus
 
-
-
+/**
+ * First time setup
+ */
 function loaded() {
     var number = 0;
-    $('iframe[name=game-frame]').contents().find("#addButton").click(function(){
+
+    /**
+     * Add Button
+     */
+    $('iframe[name=game-frame]').contents().find("#addButton").click(function () {
         number++;
         $('iframe[name=game-frame]').contents().find("#number").text(number);
-    })
-    
-    $('iframe[name=game-frame]').contents().find('#submit-score').click(function(){
-        setHighScore([{ name: "highscore", value: score}]);
     });
-};
+
+    /**
+     * Submit Score Button
+     */
+    $('iframe[name=game-frame]').contents().find('#submit-score').click(function () {
+        setHighScore([{name: "highscore", value: number}]);
+    });
+}
+;
